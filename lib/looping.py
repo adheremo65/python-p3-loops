44a7283f09ce1ce0d26e5aa4da86285b2644fba6
Abdela Umer <adheremo65@gmail.com>
2023-07-09 13:33:17 -0400

    looping lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    item = 10
+
+    while item >= 1 :
+        print(item) 
+    item -= 1
+
+print("Happy New Year!")
 
 def square_integers(int_list):
+    for item in int_list:
+       result = item*2
+       print(result)
     # code goes here!
     pass
 
 def fizzbuzz():
+  item = 100
+  while item>0:
+    print(item)
+    if ((item%3 ==0) and (item%5 ==0)):
+      print("FizzBuzz")
+    elif item%3 == 0:
+      print("Fizz")
+    elif item%5 == 0:
+      print("Buzz")
+    item -= 1 
+      
+
     # code goes here!
     pass
